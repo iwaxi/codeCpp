fc6f6a70d806007e87b19c8007cb32d34d725ca9
iwaxi <114200986+iwaxi@users.noreply.github.com>
2022-09-29 23:13:50 +0800

    Add files via upload

---
+++ 22929T.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+#include<ctime>
+#include<cstdlib>
+#include<iomanip>
+#include<cstring>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    system("pause");
+    return 0;
+}
