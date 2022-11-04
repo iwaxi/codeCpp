53a922b29b76ebfa0174e932ad3facbc3d0117ee
iwaxi <iwaxi_dy@foxmail.com>
2022-11-04 22:16:54 +0800

    2022/11/4

--- ListDouble.cpp
+++ ListDouble.cpp
@@ -21,21 +21,23 @@ public:
     void output();              // output from head to tail
     void reserveout();          // output from tail to head
     ~ListDouble();              // destroy the whole list
+    void insert(int, ElemType); // insert a new node 
 private:
     LNode<ElemType> *head; // head
     LNode<ElemType> *tail; // tail
-    static int size;       // size
+    int size;       // size
 
 };
 
 
 template<typename ElemType>         // default Construct
 ListDouble<ElemType>::ListDouble(){
+    size = 0;
     head = tail = nullptr;
 }
 
 template<typename ElemType>
-ListDouble<ElemType>::ListDouble(ElemType* data, int n): head(0),tail(0){ // parameter Construct
+ListDouble<ElemType>::ListDouble(ElemType* data, int n): size(0),head(0),tail(0){ // parameter Construct
     LNode<ElemType> *ptr = nullptr, *New = nullptr;
 
     head = new LNode<ElemType>;
@@ -57,7 +59,7 @@ ListDouble<ElemType>::ListDouble(ElemType* data, int n): head(0),tail(0){ // par
 }
 
 template<typename ElemType>
-ListDouble<ElemType>::ListDouble(int n): head(0),tail(0){ // parameter Construct
+ListDouble<ElemType>::ListDouble(int n): size(0),head(0),tail(0){ // parameter Construct
     LNode<ElemType> *ptr = nullptr, *New = nullptr;
 
     head = new LNode<ElemType>;
@@ -84,7 +86,7 @@ void ListDouble<ElemType>::output(){
     LNode<ElemType> *ptr = head->next;
     for( ; ptr; ptr = ptr->next)
         cout << ptr->value << " ";
-    cout << endl;
+    cout << "size: " << size << endl;
 }
 
 template<typename ElemType>
@@ -92,7 +94,7 @@ void ListDouble<ElemType>::reserveout(){
     LNode<ElemType> *ptr = tail;
     for( ; ptr->prev; ptr = ptr->prev)
         cout << ptr->value << " ";
-    cout << endl;
+    cout << "size: " << size << endl;
 }
 
 template<typename ElemType>
@@ -104,11 +106,42 @@ ListDouble<ElemType>::~ListDouble(){
             ptr->prev = 0;
         }    
     }
+    size = 0;
     cout << "Delete Successfully.\n";
 }
 
 template<typename ElemType>
-int ListDouble<ElemType>::size = 0;
+void ListDouble<ElemType>::insert(int index_to_insert, ElemType newValue){
+    try{
+        if(index_to_insert > size || index_to_insert < 0){
+            throw index_to_insert;
+        }
+        else{
+            LNode<ElemType>* New = new LNode<ElemType>;
+            New->value = newValue;
+            LNode<ElemType>* ptr = head->next;
+            for(int i = 0; i < size; i++){
+                if(i == index_to_insert){
+                    New->prev = ptr->prev;
+                    ptr->prev->next = New;
+                    ptr->prev = New;
+                    New->next = ptr;
+                    size ++;
+                    return;
+                }
+                ptr = ptr->next;
+            }
+            tail->next = New;
+            New->prev = tail;
+            size ++;
+            New->next = nullptr;
+        }
+    }
+    catch(int ex){
+        cout << "Error, Invalid index: " << ex << endl;
+    }
+}
+
 
 template<typename ElemType>
 ListDouble<ElemType> create(){
@@ -130,6 +163,10 @@ int main()
 
     list.output();
     list.reserveout();
+
+    list.insert(5, 11);
+    list.output();
+
     list2.output();
     list3.output();
 
