c463b711e8dc900d7e56ee9ececca9713657a056
iwaxi <iwaxi_dy@foxmail.com>
2022-11-10 00:23:24 +0800

    2022/11/9

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,34 +3,36 @@
 #include<ctime>
 using namespace std;
 
+// Quick Sort by iwaxi_dy 2022/11/10
+
 template<typename T>
-void QuickSort(T* data, int begin, int end){  // Quick Sort
+void QuickSort(T* data, int begin, int end){    // Quick Sort
     if(begin >= end)
         return;
     else{
-        int random = rand() % (end - begin);  // random base(or key)
+        int random = rand() % (end - begin + 1);// random base(or key)
         T base = data[begin + random];
         int i = 0, j = end;
         while(true){
-            while(data[i] <= base){           // left -> larger
+            while(data[i] <= base){             // left -> larger
                 i ++;
                 if(i >= end)
                     break;
             }
-            while(data[j] >= base){           // right -> lesser
+            while(data[j] >= base){             // right -> lesser
                 j --;
                 if(j <= begin)
                     break;
             }
-            if(i >= j)                        // end compare
+            if(i >= j)                          // end compare
                 break;
-            T temp = data[i];                 // swap
+            T temp = data[i];                   // swap
             data[i] = data[j];
             data[j] = temp;
         }
-        data[begin + random] = data[j];       // insert key into middle position
+        data[begin + random] = data[j];         // insert key into middle position
         data[j] = base;
-        QuickSort(data, begin, j - 1);        // take apart
+        QuickSort(data, begin, j - 1);          // take apart
         QuickSort(data, j + 1, end);
     }
 }
