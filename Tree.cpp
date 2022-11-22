55ede8e357f762669910e9b4a630929d526e2bc6
iwaxi <iwaxi_dy@foxmail.com>
2022-11-22 15:39:37 +0800

    Binary Tree 2022/11/22

--- Tree.cpp
+++ Tree.cpp
@@ -14,19 +14,22 @@ struct TreeNode
     int value;
     TreeNode* left;
     TreeNode* right;
+    TreeNode* ancestor;
 };
 
 TreeNode* CreateNode(){
     int data = 0;
     cin >> data;
     TreeNode* node = new TreeNode;
-    if(data < 0){                   // end condition
+    if(data < 0){           // end recursive
         node = nullptr;
     }
     else{
         node->value = data;
-        node->left  = CreateNode(); // priority
-        node->right = CreateNode(); // secondly
+        node->left  = CreateNode(); // left
+        if(node->left) node->left->ancestor = node;
+        node->right = CreateNode(); // right
+        if(node->right) node->right->ancestor = node;
     }
     return node;
 }
@@ -35,6 +38,14 @@ int nodeNumber(TreeNode* tr){
     if(tr)  return 1 + nodeNumber(tr->left) + nodeNumber(tr->right);
     else return 0;
 }
+
+int nodeDepth(TreeNode* tr){
+        int depth = 0;
+        if(!tr) return 0;
+        for(TreeNode* ptr = tr; ptr->ancestor; ptr = ptr->ancestor)
+            depth ++;
+        return depth;
+    }
 
 class BinaryTree
 { 
@@ -45,63 +56,70 @@ public:
     }
     void CreateTree(){
         root = CreateNode();
-    }
-    int Depth(TreeNode* tr){
-        Times ++;
-        if(tr){
-            return myMax(1 + Depth(tr->left), 1 + Depth(tr->right));
-        }
-        else
-            return 0;
-    }
+        if(root) root->ancestor = nullptr;
+    } 
     int Degree(TreeNode* tr){
         if(tr){
             int i = 0;
             if(tr->left) i++;
             if(tr->right)i++;
-            return myMax(i, myMax(Degree(tr->left), Degree(tr->right)));
+            return myMax(i, myMax(Degree(tr->left), 
+            Degree(tr->right)));
         }
         else return 0;
     }
-    bool isFull(TreeNode* tr){
-        return ((1 << Depth(tr)) - 1) == nodeNumber(tr);
+
+    int Height(TreeNode* tr){
+        Times ++;
+        if(tr){
+            return myMax(1 + Height(tr->left), 1 + Height(tr->right));
+        }
+        else
+            return 0;
     }
-    void checkFull(TreeNode* tr){
-        if(isFull(tr)) cout << "The Tree is a Full Binary Tree.\n"; 
-        else cout << "The Tree is Not a Full Binary Tree.\n";
+    void Depth(TreeNode* tr){
+        cout << "the depth of node is: " << nodeDepth(tr) << endl;
+    }
+    bool isPerfect(TreeNode* tr){
+        return ((1 << Height(tr)) - 1) == nodeNumber(tr) && nodeNumber(tr) > 1;
+    }
+    void checkPerfect(TreeNode* tr){
+        if(isPerfect(tr)) cout << "The Tree is a Perfect Binary Tree.\n"; 
+        else cout << "The Tree is Not a Perfect Binary Tree.\n";
     }
     
     void preOrderTraversal(TreeNode* tr){
         if(tr){
-            if(tr == root) cout << "\npre-order Traversal: " << endl;
+            if(tr == root) cout << "\n\npre-order Traversal: " << endl;
             cout << tr->value << " ";
             preOrderTraversal(tr->left);
             preOrderTraversal(tr->right);
-            if(!tr->left && !tr->right) cout << endl;
+            //if(!tr->left && !tr->right) cout << endl;
         }
     }
 
     void inOrderTraversal(TreeNode* tr){
         if(tr){
-            if(tr == root) cout << "\nin-order Traversal: " << endl;
+            if(tr == root) cout << "\n\nin-order Traversal: " << endl;
             inOrderTraversal(tr->left);
             cout << tr->value << " ";
             inOrderTraversal(tr->right);
-            if(!tr->left && !tr->right) cout << endl;
+            //if(!tr->left && !tr->right) cout << endl;
         }
     }
 
     void postOrderTraversal(TreeNode* tr){
         if(tr){
-            if(tr == root) cout << "\npost-order Traversal: " << endl;
+            if(tr == root) cout << "\n\npost-order Traversal: " << endl;
             postOrderTraversal(tr->left);
             postOrderTraversal(tr->right);
             cout << tr->value << " ";
-            if((!tr->left && !tr->right) || tr == root) cout << endl;
+            //if((!tr->left && !tr->right) || tr == root) cout << endl;
         }
     }
     
     void levelTraversal(TreeNode* tr){
+        if(!tr) return;
         // 1. Queue version
         myQueue<TreeNode*> queue;
         TreeNode* cur = tr;
@@ -109,15 +127,15 @@ public:
         while(!queue.isEmpty()){
             cur = queue.frontElem();  // get front
             queue.pop();              // pop front
-            if(cur == tr) cout << "\nlevel Traversal: " << endl;
+            if(cur == tr) cout << "\n\nlevel Traversal: " << endl;
             cout << cur->value << " ";
             if(cur->left) queue.push_back(cur->left);
             if(cur->right) queue.push_back(cur->right);
         }
-        cout << endl << endl;
+        cout << endl;
 
         // 2. Array version
-        int len = (1 << this->Depth(tr)) - 1;
+        int len = (1 << this->Height(tr)) - 1;
         TreeNode** level = new TreeNode* [len]{tr}; // level[0] = tr;
         for(int i = 0; 2 * i + 2 < len; i++){       // boundary !!!
             if(level[i]){
@@ -128,7 +146,7 @@ public:
         cout << "\nlevel traversal(another): " << endl;
         for(int i = 0, j = 1; i < len; i++){
             if(level[i])   cout << level[i]->value << " ";
-            if(i == (1 << j) - 2) {cout << endl;  j++;} // depth
+            if(i == (1 << j) - 2) {cout << endl;  j++;} // Height
         }
     }
 };
@@ -139,14 +157,16 @@ int main()
     tree.CreateTree();
     TreeNode* ptr = tree.root;
     cout << "\nFor the Tree input: \n\n";
-    cout << "Depth: " << tree.Depth(tree.root) << endl; 
+    cout << "Tree Height: " << tree.Height(tree.root) << endl; 
     cout << "Degree: " << tree.Degree(ptr) << endl;
     cout << "Node numbers: " << nodeNumber(ptr) << endl;
     cout << "Travel Times: " << Times << endl;
 
-    tree.checkFull(ptr);
-    tree.checkFull(ptr->right);
+    tree.checkPerfect(ptr);
+    tree.checkPerfect(ptr->right);
     
+    tree.Depth(ptr->left);
+
     tree.preOrderTraversal(ptr);
     //cout << "pre-Order times: " << preTime << endl;
     tree.inOrderTraversal(ptr);
