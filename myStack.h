a685111d5e13ec6a1d3ce356c657b42897cbc9fa
iwaxi <iwaxi_dy@foxmail.com>
2022-11-25 13:20:23 +0800

    myStack.h

---
+++ myStack.h
@@ -0,0 +1,40 @@
+#ifndef __myStack
+#define __myStack
+template<typename ElemType>
+class myStack
+{
+private:
+    ElemType* elem;
+    int sz;
+    int space;
+    void checkSize(){
+        if(sz + 1 >= space){
+            space *= 2;
+            ElemType* newElem = new ElemType[space];
+            delete[] elem;
+            elem = newElem;
+        }
+    }
+public:
+    myStack()
+        :sz(0), space(16), elem(new ElemType[space]){}
+    void push(ElemType data){
+        checkSize();
+        elem[sz++] = data;
+    }
+    ElemType get(){
+        if(sz) return elem[sz - 1];
+        else return 0;
+    }
+    void pop(){
+        if(sz) sz--;
+    }
+    bool isEmpty(){
+        return sz == 0;
+    }
+    ~myStack(){
+        delete[] elem;
+        space = sz = 0;
+    }
+};
+#endif
