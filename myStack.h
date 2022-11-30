df7c04b7df5c48b0a8916a3c7b2af768b23b5227
iwaxi <iwaxi_dy@foxmail.com>
2022-11-30 16:51:08 +0800

    myStack.h

--- myStack.h
+++ myStack.h
@@ -4,28 +4,28 @@ template<typename ElemType>
 class myStack
 {
 private:
-    ElemType* elem;
+    ElemType* elemStk;
     int sz;
     int space;
     void checkSize(){
         if(sz + 1 >= space){
             space *= 2;
-            ElemType* newElem = new ElemType[space]{};
+            ElemType* newElemStk = new ElemType[space]{};
             for(int i = 0; i < sz; i++)
-                newElem[i] = elem[i];
-            delete[] elem;
-            elem = newElem;
+                newElemStk[i] = elemStk[i];
+            delete[] elemStk;
+            elemStk = newElemStk;
         }
     }
 public:
     myStack()
-        :sz(0), space(16), elem(new ElemType[space]){}
+        :sz(0), space(16), elemStk(new ElemType[space]){}
     void push(ElemType data){
         checkSize();
-        elem[sz++] = data;
+        elemStk[sz++] = data;
     }
     ElemType get(){
-        if(sz) return elem[sz - 1];
+        if(sz) return elemStk[sz - 1];
         else return 0;
     }
     void pop(){
