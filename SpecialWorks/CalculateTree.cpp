3ce9752c7e5572d0aea06559aaac50284f8fd1f5
iwaxi <iwaxi_dy@foxmail.com>
2022-12-02 16:52:17 +0800

    Calculate Tree

--- SpecialWorks/CalculateTree.cpp
+++ SpecialWorks/CalculateTree.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<stack>
 #include<cstdio>
 using namespace std;
 
@@ -8,9 +7,50 @@ const int Minus = -2000;
 const int Multiply = -3000;
 const int Divide = -4000;
 
+
+template<typename ElemType>
+class myStack
+{
+private:
+    ElemType* elemStk;
+    int sz;
+    int space;
+    void checkSize(){
+        if(sz + 1 >= space){
+            space *= 2;
+            ElemType* newElemStk = new ElemType[space]{};
+            for(int i = 0; i < sz; i++)
+                newElemStk[i] = elemStk[i];
+            delete[] elemStk;
+            elemStk = newElemStk;
+        }
+    }
+public:
+    myStack()
+        :sz(0), space(16), elemStk(new ElemType[16]){}
+    void push(ElemType data){
+        checkSize();
+        elemStk[sz++] = data;
+    }
+    ElemType top(){
+        if(sz) return elemStk[sz - 1];
+        else return 0;
+    }
+    void pop(){
+        if(sz) sz--;
+    }
+    bool isEmpty(){
+        return sz == 0;
+    }
+    ~myStack(){
+        if(sz) delete[] elemStk;
+        space = sz = 0;
+    }
+};
+
 struct TreeNode
 {
-    double elem;
+    long long elem;
     TreeNode* left;
     TreeNode* right;
     TreeNode* parent;
@@ -21,7 +61,7 @@ class CalculateTree
 public:
     TreeNode* root;
     CalculateTree(): root(nullptr){};
-    void Insert(double data){
+    void Insert(long long data){
         TreeNode* node = new TreeNode;
         node->elem = data;
         node->left = node->right = node->parent = nullptr;
@@ -51,11 +91,11 @@ public:
             node->parent = tr;
         }
     }
-    void postOrder(TreeNode* tr, stack<double>& stk){
+    void postOrder(TreeNode* tr, myStack<double>& stk){
         if(!tr) return;
         postOrder(tr->left, stk);
         postOrder(tr->right, stk);
-        if(tr->elem > 0) stk.push(tr->elem);
+        if(tr->elem >= 0) stk.push(tr->elem);
         else{
             double a = 0, b = 0, oper = tr->elem;
             a = stk.top(); stk.pop();
@@ -75,20 +115,22 @@ public:
 int main()
 {
     while(true){
-        stack<double> stk;
+        myStack<double> stk;
         CalculateTree Tree;    
-        char str[250] = "";
-        cin.getline(str, 250);
-        if(str[0] == '0') break;
-        for(char* p = str; *p; p++){
-            int a = 0;
-            for(; *p >= '0' && *p <= '9'; p++)
-                a = a * 10 + ((*p) - '0');
-            if(*p == '+') a = Add;
-            else if(*p == '-') a = Minus;
-            else if(*p == '*') a = Multiply;
-            else if(*p == '/') a = Divide;
-            if(a) Tree.Insert(a);
+        string str;
+        getline(cin, str);
+        if(str[0] == '0' && str.size() == 1) break;
+        for(int i = 0; i < str.size(); i++){
+            long long a = -1;
+            for(; str[i] >= '0' && str[i] <= '9'; i++){
+                if(a == -1) a = 0;
+                a = a * 10 + ((str[i]) - '0');
+            }
+            if(str[i] == '+') a = Add;
+            else if(str[i] == '-') a = Minus;
+            else if(str[i] == '*') a = Multiply;
+            else if(str[i] == '/') a = Divide;
+            if(a != -1) Tree.Insert(a);
         }
         Tree.postOrder(Tree.root, stk);
         printf("%.2f\n", stk.top());
@@ -98,10 +140,163 @@ int main()
 }
 
 /*#include<iostream>
+#include<cstdio>
+#include<stack>
+#include<vector>
+using namespace std;
+
+const vector<int> Add = {-1000};
+const vector<int> Minus = {-2000};
+const vector<int> Multiply = {-3000};
+const vector<int> Divide = {-4000};
+
+vector<int> mul(vector<int> a, vector<int> b){
+    vector<int> c(a.size() + b.size());
+    int base = 1;
+    size_t i = 0;
+    for(; b[i] == 0; i ++, base *= 10){}
+    for(; i < b.size(); i++, base *= 10){
+        for(size_t j = 0; j < a.size(); j++)
+            c[j] += a[j] * b[i] * base;
+    }
+    for(size_t j = 0; j < c.size(); j++){
+        c[j + 1] += c[j] / 10;
+        c[j] %= 10;
+    }
+    int len = c.size();
+    for(; len > 0; len--){if(c[len - 1] != 0) break;}
+    c.resize(len);
+    return c;
+}
+vector<int> add(vector<int> a, vector<int> b){
+    int len = max(a.size(), b.size());
+    vector<int> c(len + 1);
+    for(int i = 0; i < len; i++){
+        if(i < min(a.size(), b.size())) c[i] = a[i] + b[i];
+        else{
+            if(a.size() > b.size()) c[i] = a[i];
+            else c[i] = b[i];
+        }
+    }
+    for(size_t j = 0; j < c.size(); j++){
+        c[j + 1] += c[j] / 10;
+        c[j] %= 10;
+    }
+    int newLen = c.size();
+    for(; newLen > 0; len--){if(c[newLen - 1] != 0) break;}
+    c.resize(newLen);
+    return c;
+}
+
+struct TreeNode
+{
+    vector<int> elem;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode* parent;
+};
+
+class CalculateTree
+{
+public:
+    TreeNode* root;
+    CalculateTree(): root(nullptr){};
+    void Insert(vector<int> data){
+        TreeNode* node = new TreeNode;
+        node->elem = data;
+        node->left = node->right = node->parent = nullptr;
+        if(!root){
+            root = node;
+            return;
+        }
+        TreeNode* tr = root;
+        if(data[0] < 0 && data[0] > -2500){
+            node->left = root;
+            root->parent = node;
+            root = node;
+        }
+        else if(data[0] < -2500){
+            while(tr->right)
+                tr = tr->right;
+            if(tr->parent) tr->parent->right = node;
+            node->parent = tr->parent;
+            tr->parent = node;
+            node->left = tr;
+            if(tr == root) root = node;
+        }
+        else{
+            while(tr->right)
+                tr = tr->right;
+            tr->right = node;
+            node->parent = tr;
+        }
+    }
+    void postOrder(TreeNode* tr, stack<vector<int>>& stk){
+        if(!tr) return;
+        postOrder(tr->left, stk);
+        postOrder(tr->right, stk);
+        if(tr->elem[0] >= 0) stk.push(tr->elem);
+        else{
+            vector<int> a, b; 
+            int oper = tr->elem[0];
+            a = stk.top(); stk.pop();
+            b = stk.top(); stk.pop();
+            if(oper > -1500)
+                stk.push(add(a, b));
+            //else if(oper > -2500)
+                //stk.push(b - a);
+            else if(oper > -3500)
+                stk.push(mul(a, b));
+            //else
+                //stk.push(b / a);
+        }
+    }
+};
+
+int main()
+{
+    while(true){
+        stack<vector<int>> stk;
+        CalculateTree Tree;    
+        string str;
+        getline(cin, str);
+        if(str[0] == '0' && str.size() == 1) break;
+        for(int i = 0; i < str.size(); i++){
+            vector<int> a = {-1};
+            for(; str[i] >= '0' && str[i] <= '9'; i++){
+                if(a[0] == -1) a[0] = str[i];
+                else a.push_back(str[i] - '0');
+            }
+            if(a[0] != -1){
+                vector<int> b(a.size());
+                for(size_t j = 0; j < a.size(); j++){
+                    b[j] = a[a.size() - 1 - j];
+                } 
+                Tree.Insert(b);
+            }
+            if(str[i] == '+') a = Add;
+            else if(str[i] == '-') a = Minus;
+            else if(str[i] == '*') a = Multiply;
+            else if(str[i] == '/') a = Divide;
+            if(a[0] < -1) Tree.Insert(a);
+        }
+        Tree.postOrder(Tree.root, stk);
+        vector<int> ans = stk.top();
+        for(int i = 0; i < ans.size(); i++)
+            cout << ans[i];
+    }
+    system("pause");
+    return 0;
+}*/
+
+
+
+
+/*#include<iostream>
 using namespace std;
 
 template<typename ElemType>
-class myStack
+class stack
 {
 private:
     ElemType* elemStk;
@@ -118,11 +313,11 @@ private:
         }
     }
 public:
-    myStack()
+    stack()
         :sz(0), space(16), elemStk(new ElemType[16]){}
-    void push(ElemType data){
+    void push(ElemType data[0]){
         checkSize();
-        elemStk[sz++] = data;
+        elemStk[sz++] = data[0];
     }
     void pop(){
         if(sz) sz--;
@@ -135,7 +330,7 @@ public:
             cout << endl;
         }
     }
-    ~myStack(){
+    ~stack(){
         if(sz) delete[] elemStk;
         space = sz = 0;
     }
@@ -160,8 +355,8 @@ public:
         front->next = rear;
         rear->next = front;
     }
-    void push(ElemType data){
-        rear->elem = data;
+    void push(ElemType data[0]){
+        rear->elem = data[0];
         LNode<ElemType>* ptr = new LNode<ElemType>;
         rear->next = ptr;
         ptr->elem = 0;
@@ -198,7 +393,7 @@ int main()
 {
     int m = 0;
     cin >> m;
-    myStack<int> * stacks = new myStack<int>[m];
+    stack<int> * stacks = new stack<int>[m];
     myQueue<int> * queues = new myQueue<int>[m];
     for(int i = 0; i < m; i++){
         int n = 0;
@@ -260,14 +455,14 @@ void Swap(T* a, T* b){
 }
 
 template<typename T>
-void max_heap(T* data, int start, int end){           
+void max_heap(T* data[0], int start, int end){           
     for(int i = start, j = 2 * i + 1; j < end; ){
-        if(j + 1 < end && data[j + 1] > data[j])  
+        if(j + 1 < end && data[0][j + 1] > data[0][j])  
             j ++;                                 
-        if(data[i] > data[j])                     
+        if(data[0][i] > data[0][j])                     
             return;                              
         else{
-            Swap(&data[i], &data[j]);            
+            Swap(&data[0][i], &data[0][j]);            
             i = j;                              
             j = 2 * i + 1;                      
         }
@@ -275,12 +470,12 @@ void max_heap(T* data, int start, int end){
 }
 
 template<typename T>
-void HeapSort(T* data, int len){
+void HeapSort(T* data[0], int len){
     for(int i = len / 2 - 1; i >= 0; i--)
-        max_heap(data, i, len);                  
+        max_heap(data[0], i, len);                  
     for(int i = len - 1; i > 0; i--){
-        Swap(&data[i], &data[0]);                
-        max_heap(data, 0, i);                    
+        Swap(&data[0][i], &data[0][0]);                
+        max_heap(data[0], 0, i);                    
     }
 }*/
 
